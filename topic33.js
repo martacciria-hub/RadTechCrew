ee48e35d8beaaf5e7980923efd630fe13767d05c
martacciria-hub <marta.cciria@gmail.com>
2026-08-24 13:33:31 +0200

    Añadir Tema 33 - Estudio radiológico de la mama

---
+++ topic33.js
@@ -0,0 +1,154 @@
+// TEMA 33. ESTUDIO RADIOLÓGICO DE LA MAMA, MX Y GALACTO MX
+const topic33Content = `TEMA 33. ESTUDIO RADIOLÓGICO DE LA MAMA, MX Y GALACTO MX
+
+El objetivo de la mamografía, es obtener imágenes de alta resolución y con buen contraste de la estructura interna de la mama, que permita detectar la patología de la mama, aunque sean tan sutiles como las microcalcificaciones en el carcinoma de mama.
+
+ANATOMÍA
+
+La mama está situada en la cara anterior del tórax sobre el pectoral mayor y se extiende desde la segunda hasta la 7ª costilla, la piel tiene un grosor de entre 0.5 y 2 mm y justo debajo se sitúa la fascia superficial, que se divide en una capa superficial y otra profunda, envolviendo la mama. La capa profunda forma la fascia retromamaria y está en contacto con la fascia del pectoral, permitiendo que la mama tenga algo de movimiento, y posibilitando que los vasos sanguíneos y linfáticos la atraviesen.
+
+La mama está dividida en compartimentos incompletos por tejido conectivo, descrito por Cooper hace 150 años y conocidos por “ligamentos de Cooper”, éstos forman una especie de picos que se unen a la piel y forman el “retinacula cutis”.
+
+Por debajo de la piel está la grasa subcutánea, que en algunas mujeres está separada del cono de parénquima de la mama y en otras no se distingue la grasa de las estructuras glandulares.
+
+La superficie del pezón es irregular y en él se encuentran los orificios de los ductos, hay entre 8 y 20 conductos mayores que se abren en el pezón, y cada uno de ellos definen el segmento de la glándula. Los conductos mayores se dilatan en la porción ampular y después se ramifican, de forma que los ductos segmentarios profundos se dividen en estructuras subsegmentarias y éstas se ramifican hasta formar acinos (unidades funcionales y secretoras de la glándula mamaria, encargadas de sintetizar y almacenar la leche durante la lactancia.)
+
+La mama sufre cambios involutivos con la edad, las células acinares de los lobulillos disminuyen, el tejido conectivo se hace fibrótico... Ésta involución probablemente comienza en la tercera o cuarta década y está bastante avanzada en la menopausia.
+
+NOMENCLATURA
+
+Es la terminología usada en mamo para identificar y localizar las lesiones, el sistema más usado es el de los cuadrantes, dividiendo la mama en 4 con el pezón como centro y referencia.
+
+TÉCNICA MX: La mamografía se hace en un equipo de RX, que consta de un generador de corriente, y una columna con un brazo giratorio que contiene el tubo de rayos, un compresor y un receptor de imagen. El tubo tiene un ánodo de molibdeno o de rodio que ayudan a tener un buen contraste del tejido mamario y pequeños puntos focales de 0.3 y 0.1 mm.
+
+Una MX de buena calidad es esencial disminuir la borrosidad haciendo una buena compresión de la mama, hay que usar parrilla antidifusora para evitar la radiación dispersa, disminuir el ruido (que depende de la calidad del haz de RX) y el grano de la película, también evitaremos artefactos que puedan estar en la piel o en la película.
+
+MX CONVENCIONAL: El detector consta de un portachasis con parrilla antidifusora, la película radiográfica es específica para mx, el chasis se mete en la reveladora y mediante el proceso químico obtenemos la placa.
+
+DIGITAL INDIRECTA: Es el mismo equipo, la diferencia la marca el chasis, la película tradicional se sustituye por una lámina luminiscente fotoestimulable. El chasis se introduce en el digitalizador que convierte la información en impulsos eléctricos y la transmite a una matriz donde se “dibuja” la imagen digital de la mx.
+
+MX DIGITAL DIRECTA: Se hace igual, pero no usamos chasis ni película ni lámina. El detector está alojado justo debajo de la mama que recibe la radiación una vez atravesada. Hay dos tipos de detectores, el fósforo-silicio amorfo o el selenio amorfo.
+
+TOMOSÍNTESIS: Es el último avance en el diagnóstico mamográfico, es una técnica tridimensional basada en una modificación de la mx digital, el tubo hace un barrido obteniendo imágenes de dos y tres dimensiones en una sola compresión, después podemos hacer reconstrucciones tridimensionales de 1 mm de grosor.
+
+PROYECCIONES
+
+Es una exploración intimidante, por eso se debe explicar bien el procedimiento, haciendo hincapié en la importancia de la compresión e insistir que a mayor compresión mejor imagen y menor dosis.
+
+Las ventajas de una buena compresión son: 1. Reducción de dosis
+
+2. Evitar el incremento de dosis acortando el T de exposición.
+
+3. Facilita el diagnóstico
+
+4. Mejora la resolución espacial al disminuir las distancias entre los tejidos.
+
+5. Reducción de la borrosidad por movimiento.
+
+⟶ Es importante que las proyecciones sean simétricas (ya que una asimetría sutil puede ser indicativo de patología)
+
+⟶ Es preferible hacer la MX, durante la fase postmenstrual (mamas menos hinchadas y menos sensibles)
+
+⟶ La Mx se hace en BIPE como norma general (si viene una paciente en silla de ruedas, etc. Se hace sentada)
+
+⟶ Algunos procedimientos intervencionistas se hacen en decúbito (la mayoría, al menos en HUB)
+
+PROYECCIONES DE RUTINA
+
+OML (oblicua medio lateral) el tubo se angula 45º, el pectoral mayor se ve cruzando la mitad superior de la placa
+
+CC (cráneo caudal) El tubo se coloca con el rayo perpendicular al suelo, todo el tejido fibroglandular queda incluido en el estudio y puede verse una pequeña parte del pectoral en el borde posterior.
+
+CALIDAD TÉCNICA DE LA MX
+
+Deben tenerse en cuenta las siguientes consideraciones:
+
+⟶ Autoevaluación del técnico y revisión por el radiólogo de la calidad de la imagen para después hacer el diagnóstico
+
+⟶ Las mamas deben estar lo más simétricas posible y proyectar la mayor cantidad de tejido posible.
+
+⟶ El pezón debe quedar de perfil (aunque no es algo necesario si con la “rotación” del pezón conseguimos ver más tejido)
+
+⟶ Las MX deben estar rotuladas en todas las proyecciones
+
+PROYECCIONES ADICIONALES
+
+CC exagerada lateralmente: para descartar sospecha de lesión en la mitad lateral de la mama. La paciente rotará un poco para que la mitad lateral de la mama se proyecte más y mejor.
+
+Cola axilar (Cleopatra): Útil para separar la cola axilar y ver lesiones en el tejido de la cola mamaria, y para confirmar la posición de la lesión, es importante ver “solo” la cola, para poder hacerla rotaremos el tubo 20º para colocarlo paralelo a la cola
+
+Proyección Valle: Útil para ver el tejido situado cerca del esternón, para hacerla se ponen las dos mamas encima del chasis, el técnico “empuja” a la paciente hacia delante, para ver la máxima proporción de tejido.
+
+Proyección ML (90º): Es la 3ª proyección más usada, si una lesión se ve en la Oblicua pero no en la CC, se hace la lateral pura para ver si la lesión es real, o una superposición de los tejidos.
+
+Se usa básicamente cuando hay que colocar un arpón, en éste caso se necesitan dos proyecciones perpendiculares entre sí, si es necesario se pide a la paciente que retraiga la mama contralateral para evitar superposición.
+
+Proyección LM (90º): Si existe una lesión en los tejidos mediales de la mama, la imagen será más nítida si se emplea ésta proyección
+
+Proyección modificada Caudo Craneal (al revés): Se usa sobretodo en mujeres con mucha cifosis, en mamas pequeñas y en varones
+
+Mama masculina: Se realiza igual que la femenina con las mismas proyecciones estándar, modificando la cráneo-caudal si es necesario.
+
+Compresión puntual y magnificación: Cuando se comprime la mama entera, el grado de compresión que se consigue, es de su porción menos comprimible. En ocasiones, el radiólogo necesita imágenes más focales. Usando un compresor más pequeño conseguimos comprimir en la zona que necesita el radiólogo, y si además quiere magnificar la zona, lo conseguimos separando la mama del detector. Esto nos ayuda a valorar mejor ciertas zonas que con la compresión estándar pueden dar lugar a sospecha de posibles lesiones. (Aunque ahora te piden una tomo, y ya no se suelen hacer las otras)
+
+PATOLOGÍAS DE LA MAMA:
+
+La mamografía, es actualmente el mejor método de cribado poblacional. Se ha establecido una clasificación con los hallazgos de la MX, conocido como BIRADS (Breast Imaging Reporting Data System) para estandarizar los resultados de la mx, unificar resultados de informes radiológicos y facilitar el análisis y la actitud terapéutica. Aunque hay que tener en cuenta que no todas las lesiones son “malas”.
+
+CC exagerada
+
+Cleopatra
+
+Valle
+
+LM (latero medial)--> el rayo incide por la axila y sale por el canalillo
+
+ML (medio lateral)--> el rayo incide en canalillo y sale por axila
+
+⟶ Lesiones Benignas:
+
+1. Mastopatía fibroquística y displasia mamaria: Estos términos se usan para definir una mama densa o con tejido fibroglandular aumentado.
+2. Fibroadenoma: Es la lesión sólida más frecuente de la mama, suele presentarse en mujeres jóvenes, son sensibles a los efectos hormonales, por lo que después de la menopausia “desaparecen”, aunque a veces se convierten en calcificaciones típicas.
+3. Quistes: Pueden encontrarse a cualquier edad, pero el pico de prevalencia se encuentra en mujeres de 30-50 años, pueden ser únicos, aunque normalmente son múltiples y suelen ser blandos, en imagen se ven como densidades redondeadas y con márgenes bien definidos.
+
+⟶ Lesiones Malignas:
+
+El carcinoma de mama es el tipo de cáncer más frecuente en la mujer, actualmente el (10%) de la población femenina padecerá un cáncer de mama.
+
+El tipo histológico de cáncer de mama más frecuente es el carcinoma ductal (90%), seguido del carcinoma lobulillar (5/10%) y por último la enfermedad de Paget (1-5%).
+
+La mayoría de los estudios, indican que las mujeres con antecedentes familiares cercanos (madre/hermana..) tienen 14 veces más riesgo que el resto de la población.
+
+En general son lesiones firmes y duras cuando son palpables y pueden estar ancladas a la piel o a las estructuras profundas de la pared torácica. En la MX, el cáncer de mama puede tener diferentes presentaciones, generalmente se ve una masa definida con márgenes espiculados o estrellados, un área de asimetría o microcalcificaciones sospechosas. Si están cerca del pezón, pueden llegar a retraerlo y si se dejan evolucionar, pueden llegar a erosionar la piel y formar masas ulceradas.
+
+PRÓTESIS MAMARIAS
+
+Hay dos localizaciones respecto al tejido mamario en que pueden colocarse:
+
+⟶ Retroglandular (el implante está detrás del tejido mamario y delante del pectoral)
+
+⟶ Retropectoral (el implante se sitúa por detrás del músculo)
+
+Para hacer la MX, se usan las proyecciones estándar solo que aplicando la técnica Eklund, que básicamente es empujar la prótesis hacia atrás y “traccionar” el tejido mamario hacia delante.
+
+PROCEDIMIENTOS INTERVENCIONISTAS
+
+⟶ PAAF (Punción-aspiración con aguja fina) usada para pequeñas lesiones, permite obtener una muestra de células para un análisis citológico.
+
+⟶ BAG (Biopsia con Aguja Gruesa) Técnica de gran rendimiento diagnóstico ante cualquier tipo de lesión, con la que se obtienen diferentes cilindros de tejido.
+
+Se puede obtener por: ⟶ mamo (estereotaxia), basada en el cálculo de coordenadas de la situación de la lesión.
+
+⟶ Eco ideal para lesiones que se ven bien por eco, es similar a la PAAF, sólo que en esta ocasión se pone anestesia local debido al calibre de las agujas.
+
+⟶ BEP (Biopsia escisional percutánea)
+
+⟶ Galactografía
+
+⟶ Localización de lesiones con arpón, es un procedimiento prequirúrgico en el que se colocan los arpones guiados por eco o por mamo, para marcar la lesión y facilitar la tarea del cirujano.
+
+⟶ Galactografía, para estudiar los conductos galactóforos a través de un contraste yodado hidrosoluble radiopaco. Se canaliza el orificio del conducto y se introduce un pequeño catéter, se introduce el contraste hasta que haya reflujo y se hacen dos proyecciones CC y LAT, sin aplicar compresión. Se indica en casos de secreción patológica por el pezón.`;
+
+if (typeof window !== 'undefined') {
+  window.topic33Content = topic33Content;
+}
